f599f9e3c4a8fd268a4db4281fd7e4367d538027
rmess001 <148206411+rmess001@users.noreply.github.com>
2023-11-27 00:49:30 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
 //AUDIO VARIABLES
-var songPlaying = 'OMS.mp3'; //current song loaded
+var songPlaying = '3.mp3'; //current song loaded
 var audio = new Audio('music/' + songPlaying); //current song loaded
 var playheadTime = 0; //playhead value relative to length of song
 var songDuration; //duration of current song
+var volumeValue = 0.5;
 
 document.addEventListener('DOMContentLoaded', function() {
     audio.addEventListener('loadeddata', function() { //get song duration
@@ -10,13 +11,14 @@ document.addEventListener('DOMContentLoaded', function() {
       });
 
     
-    audio.addEventListener('timeupdate', function() { // update the current playback position continuously    
+    audio.addEventListener('timeupdate', function() { //update the current playback position continuously
         if (playheadPressed == false) {
             playheadTime = audio.currentTime;
             playheadPosition = (audio.currentTime*580/songDuration).toFixed(6);
             playhead.style.marginLeft = (audio.currentTime*580/songDuration).toFixed(6);}
 
-            document.getElementById("timestamp").textContent = audio.currentTime.toFixed(2) + " | " + audio.duration.toFixed(2); //update timestamp
+            if (audio.currentTime.toFixed(0)%60 < 10) {document.getElementById("timestamp").textContent = Math.floor(audio.currentTime/60) + ":0" + audio.currentTime.toFixed(0)%60 + " | " + Math.floor(audio.duration/60) + ":" + audio.duration.toFixed(0)%60;} //update timestamp
+            else {document.getElementById("timestamp").textContent = Math.floor(audio.currentTime/60) + ":" + audio.currentTime.toFixed(0)%60 + " | " + Math.floor(audio.duration/60) + ":" + audio.duration.toFixed(0)%60;} //update timestamp
             if (audio.currentTime >= audio.duration) {play = false; console.log("OVER");} //turn play variable off when song has ended
       });
       
@@ -34,21 +36,22 @@ document.addEventListener('DOMContentLoaded', function() {
         playhead.addEventListener('mousedown', function() {
             console.log('Mouse pressed on the playhead!');
             // Add your code here to handle the mouse press event
-            zero = mouseX - playheadPosition;
+            Xzero = mouseX - playheadPosition;
             playheadPressed = true;
         });
         addEventListener('mouseup', function() {
             console.log('Mouse released!');
             // Add your code here to handle the mouse release event
             if (playheadPressed == true) {audio.currentTime = playheadTime; playheadPressed = false;}
-            
+            if (volumeheadPressed == true) {volumeheadPressed = false;}
         });
     }
 });
 
 
-//MOUSE MOVEMENT MAPPED TO PLAYHEAD
+//MOUSE MOVEMENT MAPPED
 var mouseX
+var mouseY
 
 document.addEventListener('DOMContentLoaded', function() {
   document.addEventListener('mousemove', updateCoordinates);
@@ -56,13 +59,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function updateCoordinates(event) {
     mouseX = event.clientX;
+    mouseY = event.clientY;
 
     if (playheadPressed == true) {
-        playheadPosition = mouseX - zero;
+        playheadPosition = mouseX - Xzero;
         if (playheadPosition < 581 && playheadPosition > -1) {playhead.style.marginLeft = playheadPosition;} //change css value
         else if (playheadPosition < 0) {playhead.style.marginLeft = 0; playheadPosition = 0;} else if (playheadPosition > 580) {playhead.style.marginLeft = 580; playheadPosition = 580;}
         playheadTime = (playheadPosition/(580/songDuration)).toFixed(6);
     } 
+
+    if (volumeheadPressed == true) {
+        volumeheadPosition = mouseY - Yzero;
+        if (volumeheadPosition > -111 && volumeheadPosition < -9) {volumehead.style.marginTop = volumeheadPosition;}
+        else if (volumeheadPosition > -10) {volumehead.style.marginTop = -10; volumeheadPosition = -10;} else if (volumeheadPosition < -110) {volumehead.style.marginTop = -110; volumeheadPosition = -110;}
+        volumeValue = ((volumeheadPosition - -10) / (-110 - -10) * (1 - 0) + 0).toFixed(6);
+        audio.volume = volumeValue;
+    }
   }
 
 //PLAY/PAUSE
@@ -77,6 +89,23 @@ document.addEventListener('DOMContentLoaded', function() {
             // Add your code here to handle the mouse press event
             if (play == true) {play = false; audio.pause();}
             else {play = true; audio.play();}
+        });
+    }    
+});
+
+//VOLUME CONTROL
+var volumeheadPosition = -60; //-10 through -110 for css value
+var volumeheadPressed = false;
+document.addEventListener('DOMContentLoaded', function() {
+    var volumehead = document.getElementById('volumehead');
+    // Check if the element with the specified ID exists
+
+    if (volumehead) {
+        volumehead.addEventListener('mousedown', function() {
+            console.log('Mouse pressed on the volumehead!');
+            // Add your code here to handle the mouse press event
+            Yzero = mouseY - volumeheadPosition;
+            volumeheadPressed = true;
         });
     }    
 });
@@ -101,58 +130,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 //METADATA MP3
-const filePath = 'music/oms.mp3';  // Replace with the actual URL of your MP3 file
-
-
-fetch(filePath)
-  .then(response => response.arrayBuffer())
-  .then(buffer => {
-    const metadata = readID3Tags(buffer);
-
-    if (metadata) {
-      console.log('Title:', metadata.title || 'N/A');
-      console.log('Authors:', metadata.authors || 'N/A');
-    } else {
-      console.log('Error reading metadata.');
-    }
-  })
-  .catch(error => {
-    console.error('Error fetching the file:', error.message);
-  });
-
-function readID3Tags(buffer) {
-  const id3Header = new TextDecoder().decode(new Uint8Array(buffer.slice(0, 3)));
-
-  if (id3Header === 'ID3') {
-    const version = new Uint8Array(buffer.slice(3, 4))[0];
-    const majorVersion = new Uint8Array(buffer.slice(4, 5))[0];
-    const minorVersion = new Uint8Array(buffer.slice(5, 6))[0];
-    const flags = new Uint8Array(buffer.slice(6, 7))[0];
-
-    let offset = 10;
-    const result = {};
-
-    while (offset < buffer.byteLength) {
-      const frameID = new TextDecoder().decode(new Uint8Array(buffer.slice(offset, offset + 4)));
-      const frameSize = new DataView(buffer).getUint32(offset + 4);
-      const frameFlags = new DataView(buffer).getUint16(offset + 8);
-
-      if (frameID === 'TIT2') {
-        const encoding = new Uint8Array(buffer.slice(offset + 10, offset + 11))[0];
-        const title = new TextDecoder().decode(new Uint8Array(buffer.slice(offset + 11, offset + frameSize)));
-        result.title = title;
-      } else if (frameID === 'TPE1') {
-        const encoding = new Uint8Array(buffer.slice(offset + 10, offset + 11))[0];
-        const authors = new TextDecoder().decode(new Uint8Array(buffer.slice(offset + 11, offset + frameSize)));
-        result.authors = authors;
-      }
-
-      offset += 10 + frameSize;
-    }
-
-    return result;
-  } else {
-    console.log('File does not have an ID3 tag.');
-    return null;
+document.addEventListener('DOMContentLoaded', function() {
+    /*
+    var jsmediatags = window.jsmediatags;
+    console.log(jsmediatags)
+    // From remote host
+jsmediatags.read("music.1.mp3", {
+  onSuccess: function(tag) {
+    console.log(tag);
+  },
+  onError: function(error) {
+    console.log(error);
   }
-}
+});
+*/
+});
+
